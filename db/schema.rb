aece9ac8ebe8369db52906ac95347df09943a065
Joshua Tang <joshua.y.tang@gmail.com>
2016-07-15 00:02:55 -0700

    Switched from sqlite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160714101914) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
     t.string   "name"
     t.text     "players"
