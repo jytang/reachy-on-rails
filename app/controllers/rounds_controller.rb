4100ca86dd823c60917cf058c1dcc2e19baae8c0
Joshua Tang <joshua.y.tang@gmail.com>
2016-07-21 15:57:00 -0700

    Simple create round

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,4 +1,13 @@
 class RoundsController < ApplicationController
   def create
+    @game = Game.find(params[:game_id])
+    @round = @game.rounds.create(round_params(@game))
+    redirect_to game_path(@game)
   end
+
+  private
+    def round_params(game)
+      params.require(:round).permit(:wind, :number, :bonus, :riichi,
+        {:scores => game.players })
+    end
 end
