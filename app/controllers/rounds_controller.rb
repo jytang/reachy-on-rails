ce385964b5d62f5441efed4f6e1d48daab41c5b5
Joshua Tang <joshua.y.tang@gmail.com>
2016-09-08 03:00:39 -0700

    Basic UI for adding next round

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -14,7 +14,20 @@ class RoundsController < ApplicationController
 
   private
     def round_params(game)
-      params.require(:round).permit(:wind, :number, :bonus, :riichi,
-        {:scores => game.players })
+      ret_params = { :wind => "S", :number => 1, :bonus => 2, :riichi => 3, :scores => @game.rounds.last.scores }
+
+      # Get user input params and make round hash
+      type = params[:type]
+      winner = params[:winner]
+      han = params[:score][:han]
+      fu = params[:score][:fu]
+
+      # Who is dealer?
+
+      # Get next round name
+
+      # Calculate hand value
+
+      return ret_params
     end
 end
