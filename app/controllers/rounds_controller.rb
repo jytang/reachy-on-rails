ace02f4cbfeded2d833b40c69b32fd313d2589d2
Joshua Tang <joshua.y.tang@gmail.com>
2016-09-09 00:08:47 -0700

    Rough implementation of adding round (tsumo only)

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,4 +1,5 @@
 class RoundsController < ApplicationController
+  include Reachy
   def create
     @game = Game.find(params[:game_id])
     @round = @game.rounds.create(round_params(@game))
@@ -18,16 +19,95 @@ class RoundsController < ApplicationController
 
       # Get user input params and make round hash
       type = params[:type]
-      winner = params[:winner]
+      winner = [ params[:winner] ]
+      loser = params[:loser]
       han = params[:score][:han]
       fu = params[:score][:fu]
+      tenpai_players = params[:tenpai_players]
 
-      # Who is dealer?
+      # Current round details
+      old_round = @game.rounds.last
+      old_wind = old_round.wind
+      old_number = old_round.number
+      old_bonus = old_round.bonus
+      old_riichi = old_round.riichi
+      old_scores = old_round.scores
 
-      # Get next round name
+      num_players = @game.players.length
 
-      # Calculate hand value
+      # Determine dealer
+      case old_number
+      when 1
+        dealer = @game.players[0]
+      when 2
+        dealer = @game.players[1]
+      when 3
+        dealer = @game.players[2]
+      when 4
+        dealer = @game.players[3]
+      end
 
-      return ret_params
+      dealer_won = winner.include?(dealer)
+
+      # Case-by-case on type of win
+      case type
+      when "tsumo"
+        puts "TSUMO"
+        losers = @game.players
+        losers -= winner
+
+        settlement = Reachy::Scoring::get_tsumo(dealer_won, [han.to_i, fu.to_i])
+
+        # Calculate bonus and riichi
+        total_bonus = old_bonus * Reachy::Scoring::P_BONUS
+        each_bonus = total_bonus / losers.length
+        total_riichi = old_riichi * Reachy::Scoring::P_RIICHI
+
+        # Update scores using old round
+        new_scores = old_scores
+
+        w = winner[0]
+        new_scores[w] += if dealer_won then settlement["nondealer"] * losers.length
+                      else settlement["dealer"] + settlement["nondealer"] * (losers.length - 1) end
+        new_scores[w] += total_bonus
+        new_scores[w] += total_riichi
+
+        losers.each do |l|
+          new_scores[l] -= settlement[l == dealer ? "dealer" : "nondealer"]
+          new_scores[l] -= each_bonus
+        end
+
+        new_wind = old_wind
+        new_number = old_number
+        new_bonus = old_bonus + 1
+        new_riichi = 0
+
+        # Adjust round wind and number if dealer did not win
+        if not dealer_won then
+          new_wind = (old_wind == "E" ? "S" : "W") if old_number == num_players
+          new_number = old_number == num_players ? 1 : old_number + 1
+          new_bonus = 0
+        end
+
+        # Update old round entry's scores
+        old_round.update(riichi: 0, scores: new_scores)
+
+      when "ron"
+        puts "RON"
+        puts loser
+        puts han
+        puts fu
+      when "ryuukyoku"
+        puts "RYUUKYOKU"
+        puts tenpai_players.length
+      when "noten"
+      when "chombo"
+        puts "CHOMBO"
+        puts loser
+      when "reset"
+      end
+
+      ret = { :wind => new_wind, :number => new_number, :bonus => new_bonus, :riichi => new_riichi, :scores => new_scores }
+      return ret
     end
 end
--- lib/reachy/scoring.rb
+++ lib/reachy/scoring.rb
@@ -1,5 +1,4 @@
 require_relative 'defines'
-require_relative 'scoretable'
 
 ##############################################
 # Scoring class
@@ -83,4 +82,168 @@ module Reachy
       end
     end
   end
+
+  # Hash of tsumo scores
+  H_TSUMO = {
+    "dealer" => {
+      "han_1" => {
+        "fu_20" => 400,
+        "fu_30" => 500,
+        "fu_40" => 700,
+        "fu_50" => 800,
+        "fu_60" => 1000,
+        "fu_70" => 1200
+      },
+      "han_2" => {
+        "fu_20" => 700,
+        "fu_30" => 1000,
+        "fu_40" => 1300,
+        "fu_50" => 1600,
+        "fu_60" => 2000,
+        "fu_70" => 2300
+      },
+      "han_3" => {
+        "fu_20" => 1300,
+        "fu_25" => 1600,
+        "fu_30" => 2000,
+        "fu_40" => 2600,
+        "fu_50" => 3200,
+        "fu_60" => 3900
+      },
+      "han_4" => {
+        "fu_20" => 2600,
+        "fu_25" => 3200,
+        "fu_30" => 3900
+      },
+      "mangan"          => 4000,
+      "haneman"         => 6000,
+      "baiman"          => 8000,
+      "sanbaiman"       => 12000,
+      "yakuman"         => 16000,
+      #"double_yakuman"  => 32000
+    },
+    "nondealer" => {
+      "han_1" => {
+        "fu_20" => { "dealer" => 400, "nondealer" => 200 },
+        "fu_30" => { "dealer" => 500, "nondealer" => 300 },
+        "fu_40" => { "dealer" => 700, "nondealer" => 400 },
+        "fu_50" => { "dealer" => 800, "nondealer" => 400 },
+        "fu_60" => { "dealer" => 1000, "nondealer" => 500 },
+        "fu_70" => { "dealer" => 1200, "nondealer" => 600 }
+      },
+      "han_2" => {
+        "fu_20" => { "dealer" => 700, "nondealer" => 400 },
+        "fu_30" => { "dealer" => 1000, "nondealer" => 500 },
+        "fu_40" => { "dealer" => 1300, "nondealer" => 700 },
+        "fu_50" => { "dealer" => 1600, "nondealer" => 800 },
+        "fu_60" => { "dealer" => 2000, "nondealer" => 1000 },
+        "fu_70" => { "dealer" => 2300, "nondealer" => 1200 }
+      },
+      "han_3" => {
+        "fu_20" => { "dealer" => 1300, "nondealer" => 700 },
+        "fu_25" => { "dealer" => 1600, "nondealer" => 800 },
+        "fu_30" => { "dealer" => 2000, "nondealer" => 1000 },
+        "fu_40" => { "dealer" => 2600, "nondealer" => 1300 },
+        "fu_50" => { "dealer" => 3200, "nondealer" => 1600 },
+        "fu_60" => { "dealer" => 3900, "nondealer" => 2000 }
+      },
+      "han_4" => {
+        "fu_20" => { "dealer" => 2600, "nondealer" => 1300 },
+        "fu_25" => { "dealer" => 3200, "nondealer" => 1600 },
+        "fu_30" => { "dealer" => 3900, "nondealer" => 2000 }
+      },
+      "mangan"          => { "dealer" => 4000, "nondealer" => 2000 },
+      "haneman"         => { "dealer" => 6000, "nondealer" => 3000 },
+      "baiman"          => { "dealer" => 8000, "nondealer" => 4000 },
+      "sanbaiman"       => { "dealer" => 12000, "nondealer" => 6000 },
+      "yakuman"         => { "dealer" => 16000, "nondealer" => 8000 },
+      #"double_yakuman"  => { "dealer" => 32000, "nondealer" => 16000 }
+    }
+  }
+
+  # Hash of ron scores
+  H_RON = {
+    "dealer" => {
+      "han_1" => {
+        "fu_20" => 1000,
+        "fu_30" => 1500,
+        "fu_40" => 2000,
+        "fu_50" => 2400,
+        "fu_60" => 2900,
+        "fu_70" => 3400
+      },
+      "han_2" => {
+        "fu_20" => 2000,
+        "fu_25" => 2400,
+        "fu_30" => 2900,
+        "fu_40" => 3900,
+        "fu_50" => 4800,
+        "fu_60" => 5800,
+        "fu_70" => 6800
+      },
+      "han_3" => {
+        "fu_20" => 3900,
+        "fu_25" => 4800,
+        "fu_30" => 5800,
+        "fu_40" => 7700,
+        "fu_50" => 9600,
+        "fu_60" => 11600
+      },
+      "han_4" => {
+        "fu_20" => 7700,
+        "fu_25" => 9600,
+        "fu_30" => 11600
+      },
+      "mangan"          => 12000,
+      "haneman"         => 18000,
+      "baiman"          => 24000,
+      "sanbaiman"       => 36000,
+      "yakuman"         => 48000,
+      #"double_yakuman"  => 96000
+    },
+    "nondealer" => {
+      "han_1" => {
+        "fu_20" => 700,
+        "fu_30" => 1000,
+        "fu_40" => 1300,
+        "fu_50" => 1600,
+        "fu_60" => 2000,
+        "fu_70" => 2300
+      },
+      "han_2" => {
+        "fu_20" => 1300,
+        "fu_30" => 2000,
+        "fu_25" => 1600,
+        "fu_40" => 2600,
+        "fu_50" => 3200,
+        "fu_60" => 3900,
+        "fu_70" => 4500
+      },
+      "han_3" => {
+        "fu_20" => 2600,
+        "fu_25" => 3200,
+        "fu_30" => 3900,
+        "fu_40" => 5200,
+        "fu_50" => 6300,
+        "fu_60" => 7700
+      },
+      "han_4" => {
+        "fu_20" => 5200,
+        "fu_25" => 6400,
+        "fu_30" => 7700
+      },
+      "mangan"          => 8000,
+      "haneman"         => 12000,
+      "baiman"          => 16000,
+      "sanbaiman"       => 24000,
+      "yakuman"         => 32000,
+      #"double_yakuman"  => 64000
+    }
+  }
+
+  # Hash of chombo scores
+  H_CHOMBO = {
+    "dealer" => 4000,
+    "nondealer" => { "dealer" => 4000, "nondealer" => 2000 }
+  }
 end
